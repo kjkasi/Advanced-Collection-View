435f07d933f9b236f62c2c5ccf0b4fab54102986
Anton Minin <vedmak17@gmail.com>
2016-01-02 13:14:38 +0300

    Fix project

--- AdvancedCollectionView/Framework/DataSources/AAPLKeyValueDataSource.h
+++ AdvancedCollectionView/Framework/DataSources/AAPLKeyValueDataSource.h
@@ -13,8 +13,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 
 
-typedef __nullable NSString * (^AAPLKeyValueTransformer)(__nullable id value);
-typedef __nullable UIImage * (^AAPLKeyValueImageTransformer)(__nullable id value);
+typedef NSString * _Nullable  (^AAPLKeyValueTransformer)(__nullable id value);
+typedef  UIImage * _Nullable  (^AAPLKeyValueImageTransformer)(__nullable id value);
 
 typedef NS_ENUM(NSInteger, AAPLKeyValueItemType) {
     AAPLKeyValueItemTypeDefault = 0,
